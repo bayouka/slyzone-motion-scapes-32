97edf61fe6bd4320e477dedd5b8bf9aa7e1f9f50
bayouka <bayoukadesbois@gmail.com>
2026-09-06 18:00:18 +0200

    Publish isolated 4b4c pilot bundle

---
+++ 4b4c-deploy/site/runtime-config.js
@@ -0,0 +1,7 @@
+window.__4B4C_CONFIG__ = Object.freeze({
+  mode: 'live',
+  supabaseUrl: 'https://wexfzhegiewhldkugtow.supabase.co',
+  supabasePublishableKey: 'sb_publishable_2XSXM2rNYsPQqJ0TzsOSgg_YDkNZwcM',
+  workspaceStorageKey: '4b4c.live.workspace.v1',
+  pollIntervalMs: 15000
+});
